a50604eb24d9dbfbd3b1285c276c2410b6b8088f
worldofcreatives <worldofcreatives.com@gmail.com>
2024-02-26 15:51:32 -0500

    first commit

---
+++ app/api/auth_routes.py
@@ -0,0 +1,66 @@
+from flask import Blueprint, request
+from app.models import User, db
+from app.forms import LoginForm
+from app.forms import SignUpForm
+from flask_login import current_user, login_user, logout_user, login_required
+
+auth_routes = Blueprint('auth', __name__)
+
+
+@auth_routes.route('/')
+def authenticate():
+    """
+    Authenticates a user.
+    """
+    if current_user.is_authenticated:
+        return current_user.to_dict()
+    return {'errors': {'message': 'Unauthorized'}}, 401
+
+@auth_routes.route('/login', methods=['POST'])
+def login():
+    form = LoginForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        user = User.query.filter(User.email == form.data['email']).first()
+        if user and user.check_password(form.data['password']):
+            login_user(user)
+            return user.to_dict()
+        else:
+            return {'errors': ['Invalid email or password.']}, 401
+    return form.errors, 401
+
+@auth_routes.route('/logout')
+def logout():
+    """
+    Logs a user out
+    """
+    logout_user()
+    return {'message': 'User logged out'}
+
+
+@auth_routes.route('/signup', methods=['POST'])
+def sign_up():
+    """
+    Creates a new user and logs them in
+    """
+    form = SignUpForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        user = User(
+            username=form.data['username'],
+            email=form.data['email'],
+            password=form.data['password']
+        )
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return user.to_dict()
+    return form.errors, 401
+
+
+@auth_routes.route('/unauthorized')
+def unauthorized():
+    """
+    Returns unauthorized JSON when flask-login authentication fails
+    """
+    return {'errors': {'message': 'Unauthorized'}}, 401
---
+++ app/models/user.py
@@ -0,0 +1,45 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+import os
+import binascii
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
+    salt = db.Column(db.String(255), nullable=False)
+    type = db.Column(db.Enum('Creator', 'Company'), default='Creator', nullable=False)
+    createdDate = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updatedDate = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    @property
+    def password(self):
+        return self.hashed_password
+
+    @password.setter
+    def password(self, password):
+        self.salt = binascii.hexlify(os.urandom(16)).decode()  # Generate a new salt
+        # Incorporate the salt with the password before hashing
+        self.hashed_password = generate_password_hash(password + self.salt)
+
+    def check_password(self, password):
+        # Use the salt from the database with the input password to check against the hashed password
+        return check_password_hash(self.hashed_password, password + self.salt)
+
+    def to_dict(self):
+        return {
+            'userId': self.id,
+            'username': self.username,
+            'email': self.email,
+            'type': self.type,
+            'createdDate': self.createdDate.isoformat(),
+            'updatedDate': self.updatedDate.isoformat(),
+        }
