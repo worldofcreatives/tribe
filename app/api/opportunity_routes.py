e983b7a18c0c8d8ed179253c45db90ac1842ab05
worldofcreatives <worldofcreatives.com@gmail.com>
2024-02-27 17:11:45 -0500

    POST a new submission

--- app/api/opportunity_routes.py
+++ app/api/opportunity_routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, jsonify, render_template
 from flask_login import login_required, current_user
-from app.models import db, Opportunity
+from app.models import db, Opportunity, Submission
 from app.forms.opportunity_form import OpportunityForm
+from app.forms.submission_form import SubmissionForm
 from sqlalchemy.exc import IntegrityError
 
 opportunity_routes = Blueprint('opportunities', __name__)
@@ -109,6 +110,47 @@ def delete_opportunity(id):
     except IntegrityError as e:
         db.session.rollback()
         return jsonify({"error": "Database error", "message": str(e)}), 500
+
+# POST /api/opportunities/:id/submit - Create a new submission
+
+@opportunity_routes.route('/<int:id>/submit', methods=['POST'])
+@login_required
+def create_submission(id):
+
+    form = SubmissionForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    opportunity = Opportunity.query.get(id)
+
+    if form.validate_on_submit():
+
+        new_submission = Submission(
+            creator_id=current_user.id,
+            opportunity_id=opportunity.id,
+            name=form.name.data,
+            notes=form.notes.data,
+            bpm=form.bpm.data,
+            collaborators=form.collaborators.data,
+        )
+        db.session.add(new_submission)
+        db.session.commit()
+
+        return jsonify(new_submission.to_dict()), 201
+    else:
+        return jsonify({'errors': form.errors}), 400
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # from flask import Blueprint, request, jsonify
--- migrations/versions/20240227_164855_.py
+++ migrations/versions/20240227_164855_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 4040e939da44
+Revision ID: 89565badb0ae
 Revises: 
-Create Date: 2024-02-27 15:02:56.810901
+Create Date: 2024-02-27 16:48:55.242885
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '4040e939da44'
+revision = '89565badb0ae'
 down_revision = None
 branch_labels = None
 depends_on = None
