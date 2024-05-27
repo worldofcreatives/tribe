a5b39fc60ae34a6289f2dd5f2280e5fb8991ad18
worldofcreatives <worldofcreatives.com@gmail.com>
2024-05-27 18:18:12 -0400

    adaptive submission button

--- react-vite/src/components/SubmissionForm/SubmissionForm.jsx
+++ react-vite/src/components/SubmissionForm/SubmissionForm.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { createNewSubmission } from '../../redux/submissions';
 import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import "./SubmissionForm.css";
 
 const SubmissionForm = ({ opportunityId }) => {
@@ -14,9 +15,11 @@ const SubmissionForm = ({ opportunityId }) => {
     file: null,
   });
   const location = useLocation();
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [successMessage, setSuccessMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const user = useSelector((state) => state.session.user);
 
   useEffect(() => {
     setSuccessMessage('');
@@ -71,6 +74,24 @@ const handleSubmit = async (e) => {
     }
 };
 
+  const renderSubmitButton = () => {
+    if (user.type === 'Company' || user.status === 'Accepted' || user.status === 'Premium') {
+      return <button className='submit-button' type="submit">Submit</button>;
+    } else if (user.status === 'Pre-Apply' || user.status === 'Denied') {
+      return (
+        <button className='submit-button' type="button" onClick={() => navigate('/apply')}>
+          Want to submit your music? Click here to apply to 7packs
+        </button>
+      );
+    } else if (user.status === 'Applied') {
+      return (
+        <button className='submit-button' type="button" disabled onClick={() => navigate('/apply')}>
+          Want to submit your music? Your 7packs application is pending
+        </button>
+      );
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
@@ -124,9 +145,7 @@ const handleSubmit = async (e) => {
           required
         />
       </div>
-      <button className='submit-button' type="submit" disabled={loading}>
-      {loading ? 'Submitting...' : 'Submit'}
-    </button>
+      {renderSubmitButton()}
     {successMessage && <div>{successMessage}</div>}
     {errorMessage && <div className="error">{errorMessage}</div>}
     </form>
@@ -134,3 +153,141 @@ const handleSubmit = async (e) => {
 };
 
 export default SubmissionForm;
+
+
+// import { useEffect, useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { createNewSubmission } from '../../redux/submissions';
+// import { useLocation } from 'react-router-dom';
+// import "./SubmissionForm.css";
+
+// const SubmissionForm = ({ opportunityId }) => {
+//   const dispatch = useDispatch();
+//   const [submissionDetails, setSubmissionDetails] = useState({
+//     name: '',
+//     notes: '',
+//     bpm: '',
+//     collaborators: '',
+//     file: null,
+//   });
+//   const location = useLocation();
+//   const [loading, setLoading] = useState(false);
+//   const [successMessage, setSuccessMessage] = useState('');
+//   const [errorMessage, setErrorMessage] = useState('');
+
+//   useEffect(() => {
+//     setSuccessMessage('');
+//     setErrorMessage('');
+//   }, [location]);
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setSubmissionDetails({
+//       ...submissionDetails,
+//       [name]: value,
+//     });
+//   };
+
+//   const handleFileChange = (e) => {
+//     setSubmissionDetails({
+//       ...submissionDetails,
+//       file: e.target.files[0],
+//     });
+//   };
+
+// const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     setLoading(true);
+//     setSuccessMessage('');
+//     setErrorMessage('');
+
+//     const formData = new FormData();
+//     formData.append('name', submissionDetails.name);
+//     formData.append('notes', submissionDetails.notes);
+//     formData.append('bpm', submissionDetails.bpm);
+//     formData.append('collaborators', submissionDetails.collaborators);
+//     if (submissionDetails.file) {
+//         formData.append('file', submissionDetails.file);
+//     }
+
+//     try {
+//         const actionResult = await dispatch(createNewSubmission(formData, opportunityId));
+//         setSuccessMessage('Submission created successfully!');
+//         // Reset submissionDetails state to clear the form
+//         setSubmissionDetails({
+//             name: '',
+//             notes: '',
+//             bpm: '',
+//             collaborators: '',
+//             file: null,
+//         });
+//     } catch (error) {
+//         setErrorMessage(error.message || 'Failed to submit the form.');
+//     } finally {
+//         setLoading(false);
+//     }
+// };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div>
+//         <label htmlFor="name">Name:</label>
+//         <input
+//           id="name"
+//           name="name"
+//           value={submissionDetails.name}
+//           onChange={handleChange}
+//           type='text'
+//           required
+//         />
+//       </div>
+//       <div>
+//         <label htmlFor="notes">Notes:</label>
+//         <textarea
+//           id="notes"
+//           name="notes"
+//           value={submissionDetails.notes}
+//           onChange={handleChange}
+//         />
+//       </div>
+//       <div>
+//         <label htmlFor="bpm">BPM:</label>
+//         <input
+//           type="number"
+//           id="bpm"
+//           name="bpm"
+//           value={submissionDetails.bpm}
+//           onChange={handleChange}
+//         />
+//       </div>
+//       <div>
+//         <label htmlFor="collaborators">Collaborators:</label>
+//         <input
+//           type="text"
+//           id="collaborators"
+//           name="collaborators"
+//           value={submissionDetails.collaborators}
+//           onChange={handleChange}
+//         />
+//       </div>
+//       <div>
+//         <label htmlFor="file">Audio:</label>
+//         <input
+//           type="file"
+//           id="file"
+//           name="file"
+//           accept=".mp3, .wav"
+//           onChange={handleFileChange}
+//           required
+//         />
+//       </div>
+//       <button className='submit-button' type="submit" disabled={loading}>
+//       {loading ? 'Submitting...' : 'Submit'}
+//     </button>
+//     {successMessage && <div>{successMessage}</div>}
+//     {errorMessage && <div className="error">{errorMessage}</div>}
+//     </form>
+//   );
+// };
+
+// export default SubmissionForm;
