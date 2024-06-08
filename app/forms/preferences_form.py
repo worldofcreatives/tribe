7dbfa870de902404c50a73d11d8b78bf5fb80232
worldofcreatives <worldofcreatives.com@gmail.com>
2024-06-08 12:11:50 -0400

    about to attempt to update availability

--- app/forms/preferences_form.py
+++ app/forms/preferences_form.py
@@ -16,17 +16,3 @@ class PreferencesForm(FlaskForm):
                 setattr(obj, name, [subfield.data for subfield in field.entries])
             else:
                 setattr(obj, name, field.data)
-
-
-
-
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, SelectField, FieldList, HiddenField
-# from wtforms.validators import DataRequired
-
-# class PreferencesForm(FlaskForm):
-#     csrf_token = HiddenField('csrf_token')
-#     event_types = FieldList(StringField('event_type', validators=[DataRequired()]), min_entries=1)
-#     activity_types = FieldList(StringField('activity_type', validators=[DataRequired()]), min_entries=1)
-#     restaurant_types = FieldList(StringField('restaurant_type', validators=[DataRequired()]), min_entries=1)
-#     group_size = SelectField('group_size', choices=[('one-on-one', 'One-on-One'), ('small', 'Small Get Together'), ('medium', 'Gathering'), ('large', 'Crowd')], validators=[DataRequired()])
